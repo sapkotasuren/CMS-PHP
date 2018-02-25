639ccbe0b255ab41b2b6c8234aca984ce773a639
sapkotasuren <sapkotasuren@hotmail.com>
2018-02-25 12:59:18 +0100

    added helper function

--- resources/functions.php
+++ resources/functions.php
@@ -13,14 +13,35 @@ if ($connection) {
 }
 
 
-
-funtion redirect($location) {
-    header("Location: $location");
+funtion redirect($location){
+header("Location: $location");
 }
 
 
-function query($sql) {
+function query($sql)
+{
     //if you want to use the connection inside the function make it global..
     global $connection;
     return mysqli_query($connection, $sql);
+}
+
+function confirm($result)
+{
+    global $connection;
+
+    if (!$result) {
+        die("QUERY FAILED " . mysqli_error($connection));
+    }
+
+}
+
+function escape_string($string)
+{
+    global $connection;
+    return mysqli_real_escape_string($connection, $string);
+}
+
+
+function fetch_array($result){
+    return mysqli_fetch_array($result);
 }
