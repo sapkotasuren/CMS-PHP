05cf2152c2ae5df5c165a4e2e1bc054ad659190e
sapkotasuren <sapkotasuren@hotmail.com>
2018-02-27 18:55:49 +0100

    new css created and dynamically linked the product description title etc

--- resources/functions.php
+++ resources/functions.php
@@ -13,10 +13,10 @@ if ($connection) {
 }
 
 
-funtion redirect($location){
-header("Location: $location");
+function redirect($location)
+{
+    header("Location: $location");
 }
-
 
 function query($sql)
 {
@@ -42,6 +42,51 @@ function escape_string($string)
 }
 
 
-function fetch_array($result){
+function fetch_array($result)
+{
     return mysqli_fetch_array($result);
+}
+
+//get products
+
+function get_products()
+{
+
+    $query = query("SELECT * FROM products");
+    confirm($query);
+    while ($row = fetch_array($query)) {
+        //heredoc
+        $product = <<<DELIMETER
+<div class="col-sm-4 col-lg-4 col-md-4">
+                        <div class="thumbnail">
+                           <a href="item.php?id={$row['product_id']}"><img src="{$row['product_image']}" alt=""></a>
+                            <div class="caption">
+                                <h4 class="pull-right">	&#8364;{$row['product_price']}</h4>
+                                <h4><a href="item.php?{$row['product_id']}">{$row['product_title']}</a>
+                                </h4>
+                                <p>test</p>
+                                <a class="btn btn-primary" target="_blank"
+                                   href="#">View
+                                    Tutorial</a>
+                            </div>
+
+                        </div>
+                    </div>
+DELIMETER;
+        echo $product;
+    }
+}
+
+
+function get_categories()
+{
+    $query = query("SELECT * FROM categories");
+    confirm($query);
+    while ($row = fetch_array($query)) {
+        $category = <<<DELIMETER
+<a href="category.php?id={$row['cat_id']}"class="list-group-item">{$row['cat_title']}</a>
+DELIMETER;
+        echo $category;
+    }
+
 }
